afb6404cfaa5c88b5390fab1a06b182b72c205ff
ShyambHauper <shyam.bhalodiya@hauper.com>
2024-08-04 00:01:44 +0530

    Add postman colletion (04/08/2024 12:01 AM)

--- index.js
+++ index.js
@@ -9,6 +9,16 @@ const { encryptResponse } = require('./middelware/encryptionMiddleware');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+// Information route
+app.get('/', (req, res) => {
+    res.status(200).json({
+        message: "This is the 8DOTS interview task.",
+        liveURL: "https://node-blog-post-hm95.vercel.app/",
+        gitURL: "https://github.com/ShyambHauper/Node-blog-post.git",
+        postmanCollection: "https://api.postman.com/collections/27182980-5e0e139d-3b7c-4b1d-a1a2-30859a897069?access_key=PMAT-01J4CR1RM3DJXZATS5PDE2KQB2",
+    });
+});
+
 // Middleware
 app.use(bodyParser.json());
 app.use(encryptResponse); // Encrypt outgoing response body
